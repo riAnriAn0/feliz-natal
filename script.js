a512d18d1877aedebf2490569a8b9a51a54a9933
riAnriAn0 <156357069+riAnriAn0@users.noreply.github.com>
2024-12-08 16:16:04 -0300

    ayualização

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ let container_caixa = document.getElementById("container_present");
 let tampa = document.getElementById("tampa");
 let caixa_baixo = document.getElementById("caixa");
 let gif = document.getElementById("gif");
+let texto = document.getElementsByTagName("h2")
 
 caixa.classList.add("animacao");
 
@@ -11,6 +12,7 @@ caixa.addEventListener("click", () => {
   caixa.classList.remove("animacao");
   container_caixa.style.display = "flex";
   caixa_comp.style.display = "none";
+  texto[0].style.display = "none";
   gif.style.display = "block";
   gif.style.animation = "gif 4s";
   tampa.style.animation = "abrir-tampa 2s";
